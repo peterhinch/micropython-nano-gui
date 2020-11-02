daba21f5a6f7c729e0c2708fe28f6280b3d50f36
Peter Hinch <peter@hinch.me.uk>
2020-11-02 16:41:22 +0000

    Temporary commit

--- gui/core/nanogui.py
+++ gui/core/nanogui.py
@@ -1,34 +1,15 @@
 # nanogui.py Displayable objects based on the Writer and CWriter classes
 # V0.3 Peter Hinch 26th Aug 2018
 
-# The MIT License (MIT)
-#
-# Copyright (c) 2018 Peter Hinch
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
+# Released under the MIT License (MIT). See LICENSE.
+# Copyright (c) 2018-2020 Peter Hinch
 
 # Base class for a displayable object. Subclasses must implement .show() and .value()
 # Has position, colors and border definition.
 # border: False no border None use bgcolor, int: treat as color
 
 import cmath
-from writer import Writer
+from gui.core.writer import Writer
 import framebuf
 import gc
 
