2b87c68ee2942ce639ef96f0a0dc52bd018c432b
Ihor Nehrutsa <Ihor.Nehrutsa@gmail.com>
2021-04-21 16:01:27 +0300

    Correct the colors in verify.py

--- verify.py
+++ verify.py
@@ -10,7 +10,9 @@ ssd.line(0, 0, ssd.width - 1, 0, YELLOW)  # Yellow top line from left to right
 ssd.line(0, 0, 0, ssd.height-1, MAGENTA)  # Magenta left line from up to down
 ssd.line(ssd.width//2, 0, ssd.width//2, ssd.height-1, RED)  # Red central line from up to down
 ssd.line(0, ssd.height//2, ssd.width - 1, ssd.height//2, BLUE)  # Blue central line left to right
-ssd.fill_rect(0, 0, ssd.width//2, 41, CYAN)  # Red square at top left
-ssd.fill_rect(0, 0, 53, ssd.height//2, GREY)  # Green square at top left
-ssd.rect(ssd.width -15, ssd.height -15, 15, 15, BLUE)  # Blue square at bottom right
+
+ssd.fill_rect(0, 0, ssd.width//2, 41, CYAN)  # Cyan square at top left
+ssd.fill_rect(0, 0, 53, ssd.height//2, GREY)  # Grey square at top left
+
+#ssd.rect(ssd.width -15, ssd.height -15, 15, 15, BLUE)  # Blue square at bottom right
 ssd.show()
