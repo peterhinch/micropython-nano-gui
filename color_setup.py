f340656c917200a7a7e8065d8ebee3f32007b423
Václav Šmilauer <eu@doxos.eu>
2021-09-05 15:44:47 +0200

    Add usdl2-based display driver

--- color_setup.py
+++ color_setup.py
@@ -1,43 +1,13 @@
+import sys
+sys.path.append('')
 # color_setup.py Customise for your hardware config
-
-# Released under the MIT License (MIT). See LICENSE.
-# Copyright (c) 2020 Peter Hinch
-
-# As written, supports:
-# Adafruit 1.5" 128*128 OLED display: https://www.adafruit.com/product/1431
-# Adafruit 1.27" 128*96 display https://www.adafruit.com/product/1673
-# Edit the driver import for other displays.
-
-# Demo of initialisation procedure designed to minimise risk of memory fail
-# when instantiating the frame buffer. The aim is to do this as early as
-# possible before importing other modules.
-
-# WIRING (Adafruit pin nos and names).
-# Pyb   SSD
-# 3v3   Vin (10)
-# Gnd   Gnd (11)
-# Y1    DC (3 DC)
-# Y2    CS (5 OC OLEDCS)
-# Y3    Rst (4 R RESET)
-# Y6    CLK (2 CL SCK)
-# Y8    DATA (1 SI MOSI)
-
-import machine
-import gc
-
-# *** Choose your color display driver here ***
-# Driver supporting non-STM platforms
-# from drivers.ssd1351.ssd1351_generic import SSD1351 as SSD
-
-# STM specific driver
-from drivers.ssd1351.ssd1351 import SSD1351 as SSD
-
-height = 96  # 1.27 inch 96*128 (rows*cols) display
-# height = 128 # 1.5 inch 128*128 display
-
-pdc = machine.Pin('Y1', machine.Pin.OUT_PP, value=0)
-pcs = machine.Pin('Y2', machine.Pin.OUT_PP, value=1)
-prst = machine.Pin('Y3', machine.Pin.OUT_PP, value=1)
-spi = machine.SPI(2)
-gc.collect()  # Precaution before instantiating framebuf
-ssd = SSD(spi, pcs, pdc, prst, height)  # Create a display instance
+if 0:
+    from drivers.terminal import Terminal
+    SSD=Terminal
+    ssd=SSD(width=196,height=128,curse=True,braille=True)
+elif 0:
+    from drivers.lvglSDL import LvglSDL as SSD
+    ssd=SSD(width=256,height=128)
+else:
+    from drivers.usdl2 import FrameBuffer as SSD
+    ssd=SSD(width=480,height=320)
